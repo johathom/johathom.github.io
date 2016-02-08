132549e6a55ac3f0409ab25bc6f227a2b482c169
Johnathan Thomas <jthomas2016@kellogg.northwestern.edu>
2016-02-07 23:59:18 -0600

    Guatamala

---
+++ chimmy/kill.js
@@ -0,0 +1,10 @@
+$(function() {
+
+  $("li").on("click", function(event) {
+    event.preventDefault();
+    var elementThatWasClicked = $(this);
+    console.log(elementThatWasClicked);
+    elementThatWasClicked.remove();
+  })
+
+})
